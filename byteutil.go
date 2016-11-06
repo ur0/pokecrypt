4c6032597cf0d08a9bb438f3c913b0efc069206d
Umang Raghuvanshi <u@umangis.me>
2016-11-06 16:45:37 +0530

    More changes

--- byteutil.go
+++ byteutil.go
@@ -1,4 +1,4 @@
-package crypto
+package pokecrypt
 
 import (
 	"reflect"
--- crypto.go
+++ crypto.go
@@ -1,32 +1,29 @@
 package pokecrypt // import "github.com/ur0/pokecrypt"
 
-import (
-  "math/rand"
-)
-
-type Pokecrypt struct{
-  msSinceStart uint32
+type Pokecrypt struct {
+	msSinceStart uint32
 }
 
-func New() *Pokecrypt {
-  return &Pokecrypt{}
+func New() Pokecrypt {
+	return Pokecrypt{}
 }
 
 // CreateIV returns a new IV from the cRand spec
-func (p *Pokecrypt) CreateIV() []byte {
-  // Re-init struct
-  p.msSinceStart = uint32(rand.Int31)
+func (p *Pokecrypt) CreateIV(ts uint32) []byte {
+	// Re-init struct
+	p.msSinceStart = ts
 
-  // This is defunct now
-  iv := make([]byte, 16)
-  return iv
+	// This is defunct now
+	iv := make([]byte, 16)
+	return iv
 }
 
 // Encrypt encrypts in with iv
-func (p *Pokecrypt) Encrypt(in []byte, iv []byte) ([]byte, error) {
-  return fpm_encrypt(in, p.msSinceStart)
+func (p *Pokecrypt) Encrypt(in, _ []byte) ([]byte, error) {
+	r := fpm_encrypt(in, p.msSinceStart)
+	return r, nil
 }
 
 func (p *Pokecrypt) Enabled() bool {
-  return true
+	return true
 }
